8d39819c2761c2ddff90e61d2363cc184fdecb21
Shankhadeep1234 <shankhadeepraj@gmail.com>
2020-07-13 19:11:01 +0530

    done with the dropdown component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import DropDown from "./components/DropDown";
 
 const item = [
   {
@@ -19,10 +20,36 @@ const item = [
   },
 ];
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+];
+
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
   return (
     <div className="ui container">
-      <Search />
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle DropDown
+      </button>
+      {showDropdown ? (
+        <DropDown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}
     </div>
   );
 };
